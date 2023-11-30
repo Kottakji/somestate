576f058737caae4b5da9dd3433964c7a76e63d42
Joris Kok <joris_kok1@hotmail.com>
2023-11-30 11:50:34 +0100

    Fix export persistent

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 export {store} from "./src/store/index.js"
 export {computed} from "./src/computed/index.js"
 export {fetched, getFetcher} from "./src/fetched/index.js"
+export {persistent} from "./src/persistent/index.js"
